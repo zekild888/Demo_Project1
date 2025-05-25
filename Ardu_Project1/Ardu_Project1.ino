c97a5f4f9ea99a49275136c22bf6bf29334747e3
Komarka <robocodeuserpro@gmail.com>
2025-05-25 13:36:36 +0300

    Update Ardu_Project1.ino

--- Ardu_Project1/Ardu_Project1.ino
+++ Ardu_Project1/Ardu_Project1.ino
@@ -2,11 +2,10 @@
 // 12:41 25.05,2025
 
 void setup() {
-  // put your setup code here, to run once:
-
+Serial.begin(9600);
+Serial.println(venom);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
